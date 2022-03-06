d80df2e10d397bf87910e2365d9861ab8802d96a
Juho Röyskö <roysko.juho@gmail.com>
2022-03-06 16:50:33 +0200

    Redirect user back to where they were when logging in or out

--- routes.py
+++ routes.py
@@ -271,16 +271,22 @@ def profile_post(username: str) -> str:
 def login() -> Union[str, Response]:
     username = ""
     password = ""
+    previous_url = request.referrer
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         success = user_service.login(username, password)
         if success:
             flash("Logged in")
-            return redirect("/")
+            return redirect(request.form["previous_url"])
         flash("Wrong username or password", "error")
 
-    return render_template("login.html", username=username, password=password)
+    return render_template(
+        "login.html",
+        username=username,
+        password=password,
+        previous_url=previous_url
+    )
 
 
 # /register
@@ -313,4 +319,4 @@ def register() -> Union[str, Response]:
 def logout() -> Response:
     user_service.logout()
     flash("Logged out")
-    return redirect("/")
+    return redirect(request.referrer)
